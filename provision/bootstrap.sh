b0f74d59b4d56b2b13586f47df726510c153c602
Andrew Thompson <andrew@tobarja.com>
2015-03-07 17:41:24 -0500

    Add composer_install wrapper to make it easy to update composer from Windows.

--- provision/bootstrap.sh
+++ provision/bootstrap.sh
@@ -15,3 +15,5 @@ a2ensite http-loyalty
 /etc/init.d/apache2 restart
 
 [ -f /usr/local/bin/composer ] || (curl -sS https://getcomposer.org/installer | php && mv composer.phar /usr/local/bin/composer)
+cp /vagrant/provision/composer_install /usr/local/bin/
+su -l -c 'composer_install' vagrant
