1511a5c6060ddbeb203e1a24993cfb52f65bad98
Andrew Thompson <andrew@tobarja.com>
2015-03-07 17:40:14 -0500

    Add .htaccess, and set up apache to use it.

--- provision/bootstrap.sh
+++ provision/bootstrap.sh
@@ -8,6 +8,10 @@ echo mysql-server mysql-server/root_password_again select vagrant | debconf-set-
 
 apt-get install -y apache2 mysql-server libapache2-mod-php5 php5-mysql
 
+cp /vagrant/provision/http-loyalty.conf /etc/apache2/sites-available/
+a2enmod rewrite
+a2dissite 000-default
+a2ensite http-loyalty
 /etc/init.d/apache2 restart
 
 [ -f /usr/local/bin/composer ] || (curl -sS https://getcomposer.org/installer | php && mv composer.phar /usr/local/bin/composer)
--- src/Home.php
+++ src/Home.php
@@ -9,6 +9,11 @@ class Home {
     public function __construct(&$app) {
         $this->app = $app;
         $app->get('/', array($this, 'home'))->name('home');
+        $app->get('/about', array($this, 'about'))->name('about');
+    }
+
+    public function about() {
+        echo "Loyalty App, created by Sandhills Community College CSC289 class, Spring 2015.";
     }
 
     public function home() {
