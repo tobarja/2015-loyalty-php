a1e9aaa0f18dc4c5ab61eeff07f53cbb250d04c6
Andrew Thompson <andrew@tobarja.com>
2015-04-11 23:01:56 -0400

    Move Edit Customer logic into Customers Controller. Reformat html & php files.

--- src/Controller/Customers.php
+++ src/Controller/Customers.php
@@ -12,10 +12,13 @@ class Customers {
         $app->get('/search/getcustomers', array($this, 'getCustomers'));
         $app->get('/customers/add', array($this, 'add'));
         $app->post('/customers/add', array($this, 'add_post'));
+        $app->get('/customers/edit/:id', array($this, 'edit'));
+        $app->post('/customers/edit/:id', array($this, 'edit_post'));
     }
 
     public function search() {
-        $this->app->render('search.html');
+        $searchText = $this->app->request->get('searchText');
+        $this->app->render('search.html', array('searchText' => $searchText));
     }
 
     public function getCustomers() {
@@ -41,7 +44,7 @@ EOT;
                 echo "<table><tr>" .
                     "<td><a href='" . $freebieUrl . "'>" . $row["FirstName"] . " " . $row["LastName"] ."</a></td>" .
                     "<td><a href='" . $freebieUrl . "'>" . $row["Telephone"]. "</a></td>" .
-                    "<td><a href='EditCustomer.php?customerid=".$row['CustomerID']."'>(Edit)</a></td>" .
+                    "<td><a href='/customers/edit/".$row['CustomerID']."'>(Edit)</a></td>" .
                     "</tr></table>";
             }
         }
@@ -52,11 +55,11 @@ EOT;
     }
 
     public function add_post() {
-        $firstname = $this->app->request->params('firstname');
-        $lastname = $this->app->request->params('lastname');
-        $telephone = $this->app->request->params('telephone');
-        $points = $this->app->request->params('points');
-        $email = $this->app->request->params('email');
+        $firstname = $this->app->request->post('firstname');
+        $lastname = $this->app->request->post('lastname');
+        $telephone = $this->app->request->post('telephone');
+        $points = $this->app->request->post('points');
+        $email = $this->app->request->post('email');
 
         $telephone = preg_replace("/[^0-9]/", "", $telephone); //remove any non-digit chars
 
@@ -93,5 +96,66 @@ EOT;
             echo "</pre>";
         }
         echo "<h2>Back to <a href='/customers/add'>Add Customer</a>";
+    }
+
+    public function edit($id) {
+        $sql = "select FirstName, LastName, Points, Telephone, Email, Points from Customers " .
+            " where CustomerID = :customerid;";
+        $statement = $this->app->db->prepare($sql);
+        $params = array('customerid' => $id);
+        $queryComplete = $statement->execute($params);
+        foreach($statement as $row){}
+        $this->app->render('customer-edit.html', array('id' => $id, 'Customer' => $row));
+    }
+
+    public function edit_post($id) {
+        $firstname = $this->app->request->post('firstname');
+        $lastname = $this->app->request->post('lastname');
+        $telephone = $this->app->request->post('telephone');
+        $points = $this->app->request->post('points');
+        $email = $this->app->request->post('email');
+
+        //$firstname = mysql_real_escape_string($firstname);
+        //$lastname = mysql_real_escape_string($lastname);
+        //$telephone = mysql_real_escape_string($telephone);
+        //$points = mysql_real_escape_string($points);
+        //$email = mysql_real_escape_string($email);
+
+        if (isset($_POST['save'])) {
+
+            $sql = <<<EOT
+                UPDATE Customers
+                SET FirstName = :fname, LastName = :lname, Points = :points, Telephone = :telephone, Email= :email
+                where CustomerID = :customerid
+EOT;
+
+            $statement = $this->app->db->prepare($sql);
+
+            $params = array('fname' => $firstname,
+                'lname' => $lastname,
+                'points' => $points,
+                'telephone' => $telephone,
+                'email' => $email,
+                'customerid' => $id);
+
+            $queryComplete = $statement->execute($params);
+        }
+
+        else if (isset($_POST['delete'])) {
+            $sql = "delete from Customers where CustomerID = :customerid";
+            $statement = $this->app->db->prepare($sql);
+
+            $params = array('customerid' => $id);
+
+            $queryComplete = $statement->execute($params);
+        }
+
+        if ($queryComplete) {
+            header("refresh:3;url=/search?searchText=$firstname $lastname");
+            echo "<h1>You will be redirected in 3 seconds.  <br>  You have updated a customer in the database, their name is $firstname $lastname , with $points points, and number $telephone, with email $email</h1>";
+        } else {
+            echo "Error: " .$sql. "<br>" . "PDO::errorInfo():\n";
+            print_r($statement->errorInfo());
+        }
     }
 }
