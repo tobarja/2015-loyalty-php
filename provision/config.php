9bcbe2800ae48ea67810ef84314eeaea93fbf009
Andrew Thompson <andrew@tobarja.com>
2015-04-15 13:16:08 -0400

    Initial Login/Logout features

--- provision/config.php
+++ provision/config.php
@@ -4,3 +4,5 @@ define("DB_HOST",'localhost');
 define("DB_USER",'root');
 define("DB_PASS",'vagrant');
 define("DB_NAME",'loyaltydb');
+
+define("SALT","cc1d35984bfe2393cc478dc32f518b86a532e37a04ab80879c7bebe776124612");
