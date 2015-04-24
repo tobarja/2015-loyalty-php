66b2a0295a6a373a9e10f35cf5bb815962e9d823
Andrew Thompson <andrew@tobarja.com>
2015-04-23 22:49:33 -0400

    Remove old employee code

--- provision/config.php
+++ provision/config.php
@@ -4,5 +4,3 @@ define("DB_HOST",'localhost');
 define("DB_USER",'root');
 define("DB_PASS",'vagrant');
 define("DB_NAME",'loyaltydb');
-
-define("SALT","cc1d35984bfe2393cc478dc32f518b86a532e37a04ab80879c7bebe776124612");
